aa14481e39f7592c8026566c1e4a722db44cdcb4
Paolo <tropaolo@gmail.com>
2022-06-04 20:07:22 +0200

    first commit

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <nav className="navbar bg-light mb-4">
+            <div className="container">
+                <span className="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    );
+}
+export default Header;
